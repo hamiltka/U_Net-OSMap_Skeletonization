e5424b148e701f49c5991e9aae6870378fa003b8
hamiltka <161067183+hamiltka@users.noreply.github.com>
2025-04-30 21:38:35 -0400

    Add files via upload

--- Advanced Model/advanced_UNet_Model.py
+++ Advanced Model/advanced_UNet_Model.py
@@ -1,15 +1,9 @@
 import torch
 import torch.nn as nn
-import torchvision
 
-def center_crop(enc_feat, target_feat):
-    _, _, h, w = target_feat.shape
-    enc_feat = torchvision.transforms.CenterCrop([h, w])(enc_feat)
-    return enc_feat
-
-class UNetMultiTask(nn.Module):
+class UNetMultiTask_NoSkip(nn.Module):
     def __init__(self, in_channels=1, out_channels=1):
-        super(UNetMultiTask, self).__init__()
+        super(UNetMultiTask_NoSkip, self).__init__()
 
         # Encoder
         self.enc1 = nn.Sequential(
@@ -39,24 +33,24 @@ class UNetMultiTask(nn.Module):
             nn.LeakyReLU(negative_slope=0.1, inplace=True)
         )
 
-        # Decoder
+        # Decoder (no skip connections, so input channels are halved)
         self.upconv3 = nn.ConvTranspose2d(512, 256, 2, stride=2)
         self.dec3 = nn.Sequential(
-            nn.Conv2d(512, 256, 3, padding=1),
+            nn.Conv2d(256, 256, 3, padding=1),
             nn.LeakyReLU(negative_slope=0.1, inplace=True),
             nn.Conv2d(256, 256, 3, padding=1),
             nn.LeakyReLU(negative_slope=0.1, inplace=True)
         )
         self.upconv2 = nn.ConvTranspose2d(256, 128, 2, stride=2)
         self.dec2 = nn.Sequential(
-            nn.Conv2d(256, 128, 3, padding=1),
+            nn.Conv2d(128, 128, 3, padding=1),
             nn.LeakyReLU(negative_slope=0.1, inplace=True),
             nn.Conv2d(128, 128, 3, padding=1),
             nn.LeakyReLU(negative_slope=0.1, inplace=True)
         )
         self.upconv1 = nn.ConvTranspose2d(128, 64, 2, stride=2)
         self.dec1 = nn.Sequential(
-            nn.Conv2d(128, 64, 3, padding=1),
+            nn.Conv2d(64, 64, 3, padding=1),
             nn.LeakyReLU(negative_slope=0.1, inplace=True),
             nn.Conv2d(64, 64, 3, padding=1),
             nn.LeakyReLU(negative_slope=0.1, inplace=True)
@@ -73,18 +67,12 @@ class UNetMultiTask(nn.Module):
         bottleneck = self.bottleneck(enc3)
 
         dec3 = self.upconv3(bottleneck)
-        enc3_crop = center_crop(enc3, dec3)
-        dec3 = torch.cat((dec3, enc3_crop), dim=1)
         dec3 = self.dec3(dec3)
 
         dec2 = self.upconv2(dec3)
-        enc2_crop = center_crop(enc2, dec2)
-        dec2 = torch.cat((dec2, enc2_crop), dim=1)
         dec2 = self.dec2(dec2)
 
         dec1 = self.upconv1(dec2)
-        enc1_crop = center_crop(enc1, dec1)
-        dec1 = torch.cat((dec1, enc1_crop), dim=1)
         dec1 = self.dec1(dec1)
 
         skeleton_pred = self.final_skeleton(dec1)
